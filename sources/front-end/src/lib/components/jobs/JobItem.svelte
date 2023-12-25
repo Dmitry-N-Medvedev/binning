4cb360555774b6a676e6c2f7980de46421c4d4e0
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2023-12-25 13:02:51 +0100

    fixed layout overflow

---
+++ sources/front-end/src/lib/components/jobs/JobItem.svelte
@@ -0,0 +1,16 @@
+<script>
+  let { jobItemId } = $props();
+</script>
+
+<style>
+  div {
+    min-height: 8rem;
+    height: 8rem;
+
+    background-color: brown;
+  }
+</style>
+
+<div>
+  job: {jobItemId}
+</div>
